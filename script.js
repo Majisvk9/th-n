dfaf2e1940e8827f8a6a0cad8bab65c13c5943a5
Majisvk9 <maji.svk9@gmail.com>
2026-02-22 10:22:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,46 @@ let count = 0;
 function addToCart(){
   count++;
   document.getElementById("cart-count").innerText = count;
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const dotsContainer = document.querySelector(".dots");
+
+let index = 0;
+
+/* Create dots */
+slides.forEach((_, i) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  if(i === 0) dot.classList.add("active");
+  dot.addEventListener("click", () => showSlide(i));
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(i){
+  index = i;
+  document.querySelector(".slides").style.transform =
+    `translateX(-${index * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+/* Auto Slide */
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 3000);
 }

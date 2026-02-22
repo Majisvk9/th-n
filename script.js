f02a1d8199ef95a8e565e83d1b2199fb2cf01179
Majisvk9 <maji.svk9@gmail.com>
2026-02-22 10:27:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,4 @@ let count = 0;
 function addToCart(){
   count++;
   document.getElementById("cart-count").innerText = count;
-const slides = document.querySelectorAll(".slide");
-const next = document.querySelector(".next");
-const prev = document.querySelector(".prev");
-const dotsContainer = document.querySelector(".dots");
-
-let index = 0;
-
 }
-
